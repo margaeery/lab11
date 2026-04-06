651988e59582d1641ff6b9a2da07a1635935e12a
margaeery <margorazhina@mail.ru>
2026-04-06 12:54:16 +0300

    feat: init app for watchtower update testing

---
+++ task5_8/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/version", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"version": "1.0.0",
+		})
+	})
+
+	r.Run(":8080")
+}
