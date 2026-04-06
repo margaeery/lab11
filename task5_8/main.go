8a3133ee055bb2ed12d671f6d9985dd191c8cb78
margaeery <margorazhina@mail.ru>
2026-04-06 13:06:46 +0300

    chore: add watchtower label to Dockerfile

--- task5_8/main.go
+++ task5_8/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
@@ -11,5 +20,25 @@ func main() {
 		})
 	})
 
-	r.Run(":8080")
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			gin.DefaultWriter.Write([]byte("server error: " + err.Error()))
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		gin.DefaultWriter.Write([]byte("server forced to shutdown: " + err.Error()))
+	}
 }
